bfbfad0278de9bdf1e6485be418c33e4455b4abd
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:35:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function make_album(artistname, albumtitle) {
+    return { artistname: artistname, albumtitle: albumtitle };
+}
+var firstAlbum = make_album("ali", "Dosti");
+var scondAlbum = make_album("Friend", "Rang e mohhabat");
+var thirdAlbum = make_album("saad", "Zindagi");
+console.log(firstAlbum);
+console.log(scondAlbum);
+console.log(thirdAlbum);
+function make_album1(artistname, albumtitle) {
+    return { artistname: artistname, albumtitle: albumtitle };
+}
+var firstAlbum1 = make_album("ali", "Dosti");
+var scondAlbum2 = make_album("Friend", "Rang e mohhabat");
+var thirdAlbum3 = make_album("saad", "Zindagi");
+console.log(firstAlbum);
+console.log(scondAlbum);
+console.log(thirdAlbum);
---
+++ main.ts
@@ -0,0 +1,20 @@
+  function make_album(artistname:string,albumtitle:string) {
+    return {artistname,albumtitle}
+  }
+  let firstAlbum = make_album("ali","Dosti")
+  let scondAlbum = make_album("Friend","Rang e mohhabat")
+  let thirdAlbum = make_album("saad","Zindagi")
+  console.log(firstAlbum);
+  console.log(scondAlbum);
+  console.log(thirdAlbum);
+
+
+  function make_album1(artistname:string,albumtitle:string) {
+    return {artistname,albumtitle}
+  }
+  let firstAlbum1 = make_album("ali","Dosti")
+  let scondAlbum2 = make_album("Friend","Rang e mohhabat")
+  let thirdAlbum3 = make_album("saad","Zindagi")
+  console.log(firstAlbum);
+  console.log(scondAlbum);
+  console.log(thirdAlbum);
